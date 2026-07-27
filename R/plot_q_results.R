515bb272ff77c1f05ff6dba3cc85c7edfd33f092
yubnkm <yubin_kim@brown.edu>
2026-07-27 17:46:11 -0400

    plot_q_results.m: without bootstrap

--- R/plot_q_results.R
+++ R/plot_q_results.R
@@ -56,5 +56,77 @@ plot_q_results <- function(result, points = 100L, show_points = FALSE) {
             )
     }
 
-    q0_plot
+    # Plot q1
+    q1_plot <- ggplot2::ggplot(
+        q1_data,
+        ggplot2::aes(x = EY0, y = EY1)
+    ) +
+        ggplot2::geom_line(color = "black") +
+        ggplot2::geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray40") +
+        ggplot2::geom_vline(
+            xintercept = c(min(q1_training_data$EY0, na.rm = TRUE), max(q1_training_data$EY0, na.rm = TRUE)),
+            linetype = "dotted",
+            color = "gray40"
+        ) +
+        ggplot2::labs(x = "EY0", y = "EY1") +
+        ggplot2::theme_classic()
+
+    if (show_points) {
+        q1_plot <- q1_plot +
+            ggplot2::geom_point(
+                data = q1_training_data,
+                ggplot2::aes(x = EY0, y = EY1),
+                inherit.aes = FALSE,
+                shape = 4,
+                size = 1,
+                stroke = 0.5,
+                alpha = 0.4
+            )
+    }
+
+    # Compare q1 and q0
+    q1_comp_data <- data.frame(
+        EY0 = q1_data$EY0,
+        EY1 = q1_data$EY1,
+        func = "q1"
+    )
+    q0_inv_data <- data.frame(
+        EY0 = q0_data$EY0,
+        EY1 = q0_data$EY1,
+        func = "q0_inverse"
+    )
+    
+    comp_data <- rbind(q1_comp_data, q0_inv_data)
+
+    comp_x_min <- max(min(q1_comp_data$EY0), min(q0_inv_data$EY0))
+    comp_x_max <- min(max(q1_comp_data$EY0), max(q0_inv_data$EY0))
+
+    comp_plot <- ggplot2::ggplot(
+        comp_data,
+        ggplot2::aes(x = EY0, y = EY1, color = func)
+    ) +
+    ggplot2::geom_line(linewidth = 0.8) +
+    ggplot2::geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray40") +
+    ggplot2::scale_color_manual(
+        name = NULL,
+        breaks = c("q1", "q0_inverse"),
+        values = c(q1 = "blue", q0_inverse = "red"),
+        labels = expression(hat(q)[1](y), hat(q)^{-1}(y))
+    ) +
+    ggplot2::coord_cartesian(xlim = c(comp_x_min, comp_x_max)) +
+    ggplot2::labs(
+        x = expression(E[Y(0) ~ "|" ~ X == x]),
+        y = expression(E[Y(1) ~ "|" ~ X == x])
+    ) +
+    ggplot2::theme_classic(base_size = 14) +
+    ggplot2::theme_classic()
+
+    return(
+        list(
+            q0_plot = q0_plot,
+            q1_plot = q1_plot,
+            comp_plot = comp_plot
+        )
+    )
+
 }
---
+++ dev/summerschool.r
@@ -0,0 +1,80 @@
+devtools::load_all(".")
+library(ggplot2)
+
+set.seed(1)
+
+data_path <- file.path("dev","ssextract_karthik.csv")
+output_dir <- file.path("dev","figures", "summer_school")
+
+level <- 0.9
+bootstrap_iter <- 100
+points <- 100
+batch_size <- 1000L
+
+data <- utils::read.csv(data_path)
+
+# Imbens and Wager only use this subsample:
+imbens_sampe <- with(
+    data,
+    mdcut01 >= -40 & mdcut01 <= 40
+    & rdcut01 >= -40 & rdcut01 <= 40
+)
+
+data <- data[imbens_sampe, , drop = FALSE]
+
+# Treatment: satisfies cutoff
+D <- as.integer(!(data$mdcut01 > 0 & data$rdcut01 > 01))
+
+# Covariates
+X_raw <- as.matrix(data[, c("mdcut01", "rdcut01")])
+colnames(X_raw) <- c("math_score", "reading_score")
+
+min_X <- apply(X_raw, 2L, min)
+max_X <- apply(X_raw, 2L, max)
+
+cutoff <- -min_X / (max_X - min_X)
+
+# normalize the covariates
+X <- sweep(X_raw, 2L, min_X, FUN = "-")
+X <- sweep(X, 2L, max_X - min_X, FUN = "/")
+colnames(X) <- c("math_score", "reading_score")
+
+# Outcomes
+Y_math <- as.numeric(data$zmscr02)
+Y_reading <- as.numeric(data$zrscr02)
+
+n <- length(Y_math)
+
+## Estimation
+# bands <- seq(0.2, 0.5, length.out = 10)
+bands <- c(0.2, 0.2)
+
+
+fit_math <- RDD_extrapolate_CV_band(
+    Y = Y_math,
+    X = X,
+    D = D,
+    kernel = "gaussian",
+    bands = bands,
+    batch_size = batch_size
+)
+
+fit_read <- RDD_extrapolate_CV_band(
+    Y = Y_reading,
+    X = X,
+    D = D,
+    kernel = "gaussian",
+    bands = bands,
+    batch_size = batch_size
+)
+
+plot_math <- plot_q_results(fit_math, points = 100L, show_points = FALSE)
+ggsave(paste0(output_dir,"/math_q0.png"), plot = plot_math$q0_plot)
+ggsave(paste0(output_dir,"/math_q1.png"), plot = plot_math$q1_plot)
+ggsave(paste0(output_dir,"/math_q_comp.png"), plot = lot_math$comp_plot)
+
+plot_read <- plot_q_results(fit_read, points = 100L, show_points = FALSE)
+ggsave(paste0(output_dir,"/read_q0.png"), plot = plot_read$q0_plot)
+ggsave(paste0(output_dir,"/read_q1.png"), plot = plot_read$q1_plot)
+ggsave(paste0(output_dir,"/read_q_comp.png"), plot = plot_read$comp_plot)
+
